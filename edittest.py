97b8b2bf3db5f9bc83b0a13ccad323d1f99387a0
emokiki <fire4ice4@gmail.com>
2023-09-04 18:00:19 +0900

    edittest

--- edittest.py
+++ edittest.py
@@ -8,12 +8,18 @@ print(b, a)
 print("python", a, b)
 
 number_school = 2394584903
+print(number_school)
 str_name = "python"
+print(str_name)
 
 bool_view = False
+print(bool_view)
 flt_pi = 3.14
+print(flt_pi)
 
 char_element = 'a'
+print(char_element)
 chars = "hello world"
+print(chars)
 
 print("number : ", number_school, "name : ", str_name, " bool : ", bool_view, " float : ", flt_pi, "character : ", char_element, " characters : ", chars)
